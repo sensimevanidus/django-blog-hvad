b1464e4dbb99da777a121101c690ec4ea5b13c38
Onur Yaman <onuryaman@gmail.com>
2013-06-28 23:39:33 +0300

    Implement single post details view

--- blog_hvad/urls.py
+++ blog_hvad/urls.py
@@ -3,4 +3,5 @@ from django.utils.translation import ugettext_lazy as _
 
 urlpatterns = patterns('blog_hvad.views',
     url(_(r'^posts/?$'), 'posts', name='posts'),
+    url(_(r'^post/(?P<slug>.+)/?$'), 'post', name='post'),
 )
--- blog_hvad/views.py
+++ blog_hvad/views.py
@@ -1,8 +1,16 @@
 # -*- coding: utf-8 -*-
-from django.shortcuts import render_to_response
+from django.http import Http404
+from django.shortcuts import render_to_response, get_object_or_404
 from django.template import RequestContext
 from .models import BlogPost
 
 def posts(request):
     posts = BlogPost.objects.all()
     return render_to_response('blog_hvad/list.html', {'posts': posts}, context_instance=RequestContext(request))
+
+def post(request, slug):
+    try:
+        post = BlogPost.objects.language().get(slug=slug,)
+    except BlogPost.DoesNotExist:
+        raise Http404
+    return render_to_response('blog_hvad/post.html', {'post': post}, context_instance=RequestContext(request))
