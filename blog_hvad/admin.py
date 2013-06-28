b668acf523884d5d544674f1b0aa6b427a665a9c
Onur Yaman <onuryaman@gmail.com>
2013-06-29 01:40:59 +0300

    Implement blog categories feature (also add south migrations)

--- blog_hvad/admin.py
+++ blog_hvad/admin.py
@@ -1,9 +1,30 @@
 from django.contrib import admin
 from hvad.admin import TranslatableAdmin
-from .models import BlogPost
+from .models import BlogCategory, BlogPost
+
+class BlogCategoryAdmin(TranslatableAdmin):
+    class Meta:
+        model = BlogCategory
+
+    #
+    # Workaround for prepopulated_fields and fieldsets from here:
+    # https://github.com/KristianOellegaard/django-hvad/issues/10#issuecomment-5572524
+    #
+    def __init__(self, *args, **kwargs):
+        super(BlogCategoryAdmin, self).__init__(*args, **kwargs)
+        self.prepopulated_fields = {'slug': ('name',)}
 
 class BlogPostAdmin(TranslatableAdmin):
     class Meta:
         model = BlogPost
 
+    #
+    # Workaround for prepopulated_fields and fieldsets from here:
+    # https://github.com/KristianOellegaard/django-hvad/issues/10#issuecomment-5572524
+    #
+    def __init__(self, *args, **kwargs):
+        super(BlogPostAdmin, self).__init__(*args, **kwargs)
+        self.prepopulated_fields = {'slug': ('title',)}
+
+admin.site.register(BlogCategory, BlogCategoryAdmin)
 admin.site.register(BlogPost, BlogPostAdmin)
--- blog_hvad/models.py
+++ blog_hvad/models.py
@@ -5,6 +5,27 @@ from django.utils.translation import ugettext_lazy as _
 from hvad.models import TranslatableModel, TranslatedFields
 
 
+class BlogCategory(TranslatableModel):
+    """
+    Represents a blog category.
+
+    """
+
+    created_at = models.DateTimeField(_('created at'), auto_now_add=True,)
+    updated_at = models.DateTimeField(_('updated at'), auto_now=True,)
+
+    translations = TranslatedFields(
+        name=models.CharField(_('name'), max_length=127,),
+        slug=models.SlugField(_('slug'), unique=True,),
+    )
+
+    class Meta:
+        verbose_name = _('blog category')
+        verbose_name_plural = _('blog categories')
+
+    def __unicode__(self):
+        return u'%s' % self.name
+
 class BlogPost(TranslatableModel):
     """
     Represents a blog post.
@@ -15,6 +36,7 @@ class BlogPost(TranslatableModel):
     created_at = models.DateTimeField(_('created at'), auto_now_add=True,)
     updated_at = models.DateTimeField(_('updated at'), auto_now=True,)
     is_published = models.BooleanField(_('is published?'), default=False,)
+    categories = models.ManyToManyField(BlogCategory, verbose_name=_('category'), related_name='blog_posts',)
 
     translations = TranslatedFields(
         title=models.CharField(_('title'), max_length=127,),
