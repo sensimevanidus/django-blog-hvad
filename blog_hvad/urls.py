b532087144a00590e47bab6d2341706ba7d53e7e
Onur Yaman <onuryaman@gmail.com>
2013-08-15 04:00:00 +0300

    Add support for listing blog posts under a given category

--- blog_hvad/urls.py
+++ blog_hvad/urls.py
@@ -3,5 +3,6 @@ from django.utils.translation import ugettext_lazy as _
 
 urlpatterns = patterns('blog_hvad.views',
     url(_(r'^posts/?$'), 'posts', name='posts'),
+    url(_(r'^posts/(?P<category_slug>.+)/?$'), 'category_posts', name='category_posts'),
     url(_(r'^post/(?P<slug>.+)/?$'), 'post', name='post'),
 )
--- blog_hvad/views.py
+++ blog_hvad/views.py
@@ -1,12 +1,20 @@
 # -*- coding: utf-8 -*-
 from django.http import Http404
-from django.shortcuts import render_to_response, get_object_or_404
+from django.shortcuts import render_to_response
 from django.template import RequestContext
-from .models import BlogPost
+from .models import BlogPost, BlogCategory
 
 def posts(request):
     posts = BlogPost.objects.language().all()
-    return render_to_response('blog_hvad/list.html', {'posts': posts}, context_instance=RequestContext(request))
+    return render_to_response('blog_hvad/posts.html', {'posts': posts}, context_instance=RequestContext(request))
+
+def category_posts(request, category_slug):
+    try:
+        category = BlogCategory.objects.language().get(slug__exact=category_slug)
+        posts = category.blog_posts.all()
+        return render_to_response('blog_hvad/category_posts.html', {'category': category, 'posts': posts}, context_instance=RequestContext(request))
+    except BlogCategory.DoesNotExist:
+        raise Http404
 
 def post(request, slug):
     try:
